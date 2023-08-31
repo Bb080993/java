d0803a7bac68ebb71b5b414533e2f3f43828a4b1
Brittany Francis <brittanyfrancis200@gmail.com>
2023-08-30 21:15:23 -0400

    Practice Instantiating Class in Java

---
+++ fundamentals/practice_question/Test.java
@@ -0,0 +1,8 @@
+public class Test{
+    public static void main(String[] args){
+        User newUser= new User();
+        String makeUser=newUser.getName("Brittany");
+        System.out.print(makeUser);
+    }
+
+}
---
+++ fundamentals/practice_question/User.java
@@ -0,0 +1,7 @@
+public class User{
+
+    public String getName(String name){
+        System.out.println(name);
+        return name;
+    }
+}
